a9a496b54e344dd82ef6cf32bb318a848f4ff3e1
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2020-03-24 13:57:32 +0100

    Ejer1 - 5

---
+++ Ejer3LinqPractice_SecondTry/Coche.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer3LinqPractice_SecondTry
+{
+    public class Location
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+
+    public class Coche : Location
+    {
+        public int id { get; set; }
+        public string Maker { get; set; }
+        public string Model { get; set; }
+        public int? Year { get; set; }
+        public string Color { get; set; }
+        public Location Location { get; set; }
+    }
+}
--- Ejer3LinqPractice_SecondTry/Program.cs
+++ Ejer3LinqPractice_SecondTry/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,65 @@ namespace Ejer3LinqPractice_SecondTry
 {
     class Program
     {
+        static List<Coche> listaCoche = new List<Coche>();
         static void Main(string[] args)
         {
+            Ejer3();
+        }
+
+        static List<Coche> Ejer1() //Crea la clase para poder cargar el Json y cargalo.
+        {
+            using (StreamReader lector = new StreamReader("Cars.json"))
+            {
+                string json = lector.ReadToEnd();
+                List<Coche> resultado = JsonConvert.DeserializeObject<List<Coche>>(json);
+                return resultado;
+            }
+        }
+
+        static void Ejer2() //Muestra los distintos Fabricantes, sin duplicar ningún fabricante y poniendo inicialmente "Fabricante :" en todos los elementos.
+        {
+            var dist = listaCoche.GroupBy(x => x.Maker).Select(y => y.First());
+            foreach (var i in dist)
+            {
+                Console.WriteLine("Fabricante :" + i.Maker);
+            }
+        }
+
+        static void Ejer3() //Muestra los diferentes colores indicando Maker y Modelo también.
+        {
+            var ajar = listaCoche.GroupBy(x=> x.Color).Select(y => y.First());
+            foreach (var i in ajar)
+            {
+                Console.WriteLine("Color :" + i.Color + "Fabricante :" + i.Maker);
+            }
+        }
+
+        static void Ejer4() //Muestra fabricante y modelo. Del color Verde.
+        {
+            var beti = listaCoche.Where(x => x.Color == "Verde");
+            foreach (var i in beti)
+            {
+                Console.WriteLine("Frabricante :" + i.Maker + "Modelo :" + i.Model);
+            }
+        }
+
+        static void Ejer5(double latitud, double longitud) //Permite al usuario introducir una latitud y una longitud. 
+                                                           //Indica al usuario si encuentra un coche del año 1992 dentro de esa latitud y longitud facilitada.
+        {
+            var buscar = listaCoche.Where(x => x.Latitude == latitud && x.Longitude == longitud);
+            foreach (var i in buscar)
+            {
+                if (i.Year == 1992)
+                {
+                    Console.WriteLine("Hay un coche del año 1992");
+                }
+                else
+                {
+                    Console.WriteLine("No hay un coche del año 1992");
+                }
+            }
+
         }
     }
 }
